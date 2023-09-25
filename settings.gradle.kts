43caf8ee203db973ea8a9558765b1a70a33d311b
Karla Jazmin Zuñiga Moreno <karly33zm@gmail.com>
2023-09-25 13:34:05 -0600

    PROYECTO 1 2023-2

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "P1_ISC_7S21_KARLA"
+include(":app")
+ 
